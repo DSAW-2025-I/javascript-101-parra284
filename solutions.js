aed5369810d1049e834788bb50d993103b82e2c1
Alejandro Parra <alejandropaga@unisabana.edu.co>
2025-02-16 20:07:46 -0500

    Fix message of error

--- solutions.js
+++ solutions.js
@@ -1,10 +1,17 @@
 // Sum of Two Numbers
 function sum(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+  console.log("Los datos de entrada no son adecuados");
+  }
   return a + b;
 }
 
 // Factorial of a Number
 function factorial(n) {
+  if (typeof n !== "number" || n < 0 || !Number.isInteger(n)) {
+    console.log("Los datos de entrada no son adecuados");
+  }
+
   let result = 1;
 
   for (let i = n; i > 1; i--) {
@@ -16,6 +23,10 @@ function factorial(n) {
 
 // Find the Largest Number
 function findLargest(arr) {
+  if (!Array.isArray(arr) || arr.length === 0 || arr.some(num => typeof num !== "number")) {
+    console.log("Los datos de entrada no son adecuados");
+  }
+
   let max = arr[0];
 
   for (let i = 1; i < arr.length; i++) {
@@ -27,6 +38,10 @@ function findLargest(arr) {
 
 // Count Vowels in a String
 function countVowels(str) {
+  if (typeof str !== "string") {
+    console.log("Los datos de entrada no son adecuados");
+  }
+
   let vowels = ["a", "e", "i", "o", "u"]
   let count = 0;
 
@@ -39,6 +54,10 @@ function countVowels(str) {
 
 // Check if a Number is Prime
 function isPrime(n) {
+  if (typeof n !== "number" || n < 2 || !Number.isInteger(n)) {
+    console.log("Los datos de entrada no son adecuados");
+  }
+
   if (n < 2) return false;
   if (n == 2 || n == 3) return true;
   if (n % 2 == 0 || n % 3 == 0) return false;
